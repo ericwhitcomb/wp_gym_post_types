2448ad7a4630fa16c15a77c01b2e7545da490538
Eric Whitcomb <eric.whitcomb@gmail.com>
2021-09-03 02:52:22 -0600

    Add instructor post type

--- wp_gym_post_types.php
+++ wp_gym_post_types.php
@@ -2,7 +2,7 @@
 /*
  * Plugin Name: WP Gym - Post Types
  * Plugin URI: 
- * Description: Adds a new Post Type into WordPress
+ * Description: Add Custom Post Types into WordPress for WP Gym
  * Version: 1.0.0
  * Author: Eric Whitcomb
  * Author URI: http://www.ericwhitcomb.com/
@@ -11,7 +11,7 @@
 
 if (!defined('ABSPATH')) die();
 
-// Register new Custom Post Type
+// Register post type for classes
 function wp_gym_class_post_type() {
 
 	$labels = array(
@@ -67,4 +67,61 @@ function wp_gym_class_post_type() {
 
 }
 add_action( 'init', 'wp_gym_class_post_type', 0 );
+
+// Register post type for instructors
+function wp_gym_instructor_post_type() {
+
+	$labels = array(
+		'name'                  => _x( 'Instructors', 'Post Type General Name', 'wp_gym' ),
+		'singular_name'         => _x( 'Instructor', 'Post Type Singular Name', 'wp_gym' ),
+		'menu_name'             => __( 'Instructors', 'wp_gym' ),
+		'name_admin_bar'        => __( 'Instructor', 'wp_gym' ),
+		'archives'              => __( 'Archive', 'wp_gym' ),
+		'attributes'            => __( 'Attributes', 'wp_gym' ),
+		'parent_item_colon'     => __( 'Parent Instructor', 'wp_gym' ),
+		'all_items'             => __( 'All Instructors', 'wp_gym' ),
+		'add_new_item'          => __( 'Add Instructor', 'wp_gym' ),
+		'add_new'               => __( 'Add Instructor', 'wp_gym' ),
+		'new_item'              => __( 'New Instructor', 'wp_gym' ),
+		'edit_item'             => __( 'Edit Instructor', 'wp_gym' ),
+		'update_item'           => __( 'Update Instructor', 'wp_gym' ),
+		'view_item'             => __( 'View Instructor', 'wp_gym' ),
+		'view_items'            => __( 'View Instructors', 'wp_gym' ),
+		'search_items'          => __( 'Search Instructor', 'wp_gym' ),
+		'not_found'             => __( 'Not Found', 'wp_gym' ),
+		'not_found_in_trash'    => __( 'Not Found in Trash', 'wp_gym' ),
+		'featured_image'        => __( 'Featured Image', 'wp_gym' ),
+		'set_featured_image'    => __( 'Save Featured Image', 'wp_gym' ),
+		'remove_featured_image' => __( 'Remove Featured Image', 'wp_gym' ),
+		'use_featured_image'    => __( 'Use as Featured Image', 'wp_gym' ),
+		'insert_into_item'      => __( 'Insert in Instructor', 'wp_gym' ),
+		'uploaded_to_this_item' => __( 'Add in Instructor', 'wp_gym' ),
+		'items_list'            => __( 'List Instructors', 'wp_gym' ),
+		'items_list_navigation' => __( 'Navigate to Instructors', 'wp_gym' ),
+		'filter_items_list'     => __( 'Filter Instructors', 'wp_gym' ),
+	);
+	$args = array(
+		'label'                 => __( 'Instructors', 'wp_gym' ),
+		'description'           => __( 'Instructors for website', 'wp_gym' ),
+		'labels'                => $labels,
+		'supports'              => array( 'title', 'editor', 'thumbnail' ),
+		'hierarchical'          => false,
+		'public'                => true,
+		'show_ui'               => true,
+		'show_in_menu'          => true,
+        'menu_position'         => 7,
+        'menu_icon'             => 'dashicons-admin-users',
+		'show_in_admin_bar'     => true,
+		'show_in_nav_menus'     => true,
+		'can_export'            => true,
+		'has_archive'           => false,
+		'exclude_from_search'   => false,
+		'publicly_queryable'    => true,
+		'capability_type'       => 'page',
+    'show_in_rest'          => false // set to true to enable gutenberg
+	);
+	register_post_type( 'wp_gym_instructors', $args );
+
+}
+add_action( 'init', 'wp_gym_instructor_post_type', 0 );
 ?>
